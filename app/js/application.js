559d39f76433b5f748176baca00f39c302073ece
Rezki Bangsawan <rbangsawan@gmail.com>
2013-12-19 13:24:38 +0700

    Sticky Footer Script Started.

--- app/js/application.js
+++ app/js/application.js
@@ -10095,6 +10095,39 @@ if ( typeof window === "object" && typeof window.document === "object" ) {
     return Response;
 
 }));
+$(window).bind("load", function() {
+
+  var footerHeight = 0,
+  footerTop = 0,
+  $footer = $(".footer");
+
+  function positionFooter() {
+
+    footerHeight = $footer.height();
+    footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
+
+    if ( ($(document.body).height()+footerHeight) < $(window).height()) {
+      $footer.css({
+        position: "absolute"
+      }).animate({
+        top: footerTop
+      });
+    } else {
+      $footer.css({
+      position: "static"
+    });
+    }
+
+  }
+
+  positionFooter();
+
+  $(window)
+  .scroll(positionFooter)
+  .resize(positionFooter);
+
+});
+
 ;( function( $, R ) {
 	R.ready( function() {
 
