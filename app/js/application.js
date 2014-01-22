49ab14ca4b909867d1b6b06deae6824076989c4b
Rezki Bangsawan <rbangsawan@gmail.com>
2014-01-22 17:36:29 +0700

    Common Sidebar Timeline, done.

--- app/js/application.js
+++ app/js/application.js
@@ -10097,21 +10097,32 @@ if ( typeof window === "object" && typeof window.document === "object" ) {
 }));
 ;( function( $, R ) {
 
-    var $commonSidebarsNav = $('.js.common-sidebars-nav'),
-        $comsideFloatButton = $('.js.comside-float-button'),
-        $comsideLinkToggle = $('.js.comside-link.toggle')
+    var $commonSidebarsNav      = $('.js.common-sidebars-nav'),
+        $commonSidebarsTimeline = $('.js.common-sidebars-timeline'),
+        $comsideFloatButton     = $('.js.comside-float-button'),
+        $comsideLinkToggle      = $('.js.comside-link.toggle')
         ;
 
     $comsideFloatButton.on('click', function(e) {
         e.preventDefault();
-        $comsideFloatButton.toggleClass('is-shown');
-        $commonSidebarsNav.toggleClass('is-shown');
+        if( $('.js.common-sidebars-nav').length > 0 ) {
+            $comsideFloatButton.toggleClass('is-shown');
+            $commonSidebarsNav.toggleClass('is-shown');
+        } else if( $('.js.common-sidebars-timeline').length > 0 ) {
+            $comsideFloatButton.toggleClass('is-shown');
+            $commonSidebarsTimeline.toggleClass('is-shown');
+        }
     });
 
     $comsideLinkToggle.on('click', function(e) {
         e.preventDefault();
-        $comsideFloatButton.toggleClass('is-shown');
-        $commonSidebarsNav.toggleClass('is-shown');
+        if( $('.js.common-sidebars-nav').length > 0 ) {
+            $comsideFloatButton.toggleClass('is-shown');
+            $commonSidebarsNav.toggleClass('is-shown');
+        } else if( $('.js.common-sidebars-timeline').length > 0 ) {
+            $comsideFloatButton.toggleClass('is-shown');
+            $commonSidebarsTimeline.toggleClass('is-shown');
+        }
     });
 
 })(jQuery, Response);
